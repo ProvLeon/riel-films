c8fdf4188254d3efeecc05590f42fc87a57ce7f7
ProvLeon <providence.leonard@gmail.com>
2025-03-19 13:00:28 +0000

    Replace img tags with Next.js Image components

--- components/UI/GradCard.tsx
+++ components/UI/GradCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const GradCard = ({ grad }: any) => {
@@ -5,7 +6,7 @@ const GradCard = ({ grad }: any) => {
     <div className="flex flex-wrap items-start w-full text-lg font-light leading-8 text-neutral-600">
       <div className="flex flex-col flex-1 shrink justify-center p-4 basis-0 min-w-60">
         <div className="w-full">
-          <img
+          <Image
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/b462b65a33170fee2ce0fb8caa34c2cd3ed64c268d3d85594f7d54e15035bfd6?placeholderIfAbsent=true&apiKey=931f40127e7944988057f598479fa95c"
             className="object-contain w-full aspect-[1.5]"
             alt="Graduate 1"
@@ -22,7 +23,7 @@ const GradCard = ({ grad }: any) => {
       </div>
       <div className="flex flex-col flex-1 shrink justify-center p-4 basis-0 min-w-60">
         <div className="w-full">
-          <img
+          <Image
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/9c307e7d48b564b8a087b418f5b535214f1c4ef3ebbde6f93541ccabbfc0e539?placeholderIfAbsent=true&apiKey=931f40127e7944988057f598479fa95c"
             className="object-contain w-full aspect-[1.5]"
             alt="Graduate 2"
@@ -38,7 +39,7 @@ const GradCard = ({ grad }: any) => {
       </div>
       <div className="flex flex-col flex-1 shrink justify-center p-4 basis-0 min-w-60">
         <div className="w-full">
-          <img
+          <Image
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/15304fc83eebe055841cefdcfe29de60e395c8fcf675237edb5d98b052f5ce6b?placeholderIfAbsent=true&apiKey=931f40127e7944988057f598479fa95c"
             className="object-contain w-full aspect-[1.5]"
             alt="Graduate 3"
